c07b622b59f2fb4735f097b68cfe68b4929dab5f
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-09-29 18:13:48 +0530

    Making the Grid of the videos on the home page !

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,34 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { YOUTUBE_API } from "../utils/common";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return <div>Video Container</div>;
+  // Setting this in the state
+  const [videosInfo, setVideosInfo] = useState([]);
+
+  useEffect(() => {
+    getAPIdataFromYT();
+  }, []);
+
+  if (videosInfo === null) {
+    return <h1> Loading the data </h1>;
+  }
+
+  // Getting the data from the API
+  const getAPIdataFromYT = async () => {
+    const response = await fetch(YOUTUBE_API);
+    const data = await response.json();
+    setVideosInfo(data.items);
+    console.log(data.items);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videosInfo.map((video) => (
+        <VideoCard info={video} />
+      ))}
+    </div>
+  );
 };
 
 export default VideoContainer;
---
+++ src/utils/common.js
@@ -0,0 +1,5 @@
+export const API_KEY = "AIzaSyDHyUkmamy8MghDrBL1vgqi1HLsl0wpr6k";
+
+export const YOUTUBE_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+  API_KEY;
