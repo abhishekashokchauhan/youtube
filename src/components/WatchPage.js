d5ac536562129fd81ec8d3bc3223d426931d9905
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-10-06 15:11:26 +0530

    Implemented n level comments.

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -13,16 +14,21 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="h-5 p-5">
-      <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+    <div className="h-5 p-5 flex flex-col w-full">
+      <div className="">
+        <iframe
+          width="560"
+          height="315"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="m-2 p-2">
+        <CommentsContainer />
+      </div>
     </div>
   );
 };
